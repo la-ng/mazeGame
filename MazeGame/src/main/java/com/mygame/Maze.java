ca9ddd9c1062e1c68b56422bb071f87a0240063a
la-ng <law.ng78@gmail.com>
2023-04-14 02:15:32 -0400

    Initial maze design, merging some changes together
    This was a lot, hopefully everything works out

--- MazeGame/src/main/java/com/mygame/Maze.java
+++ MazeGame/src/main/java/com/mygame/Maze.java
@@ -24,13 +24,7 @@ public class Maze {
      * @param space where collision objects should be added
      */
 
-    public static void createWorld(Node rootNode, AssetManager assetManager, PhysicsSpace space, String type) {
-        PointLight light = new PointLight();
-        light.setColor(ColorRGBA.Yellow);
-        light.setRadius(4f);
-        
-        rootNode.addLight(light);
-        
+    public static float[] createWorld(Node rootNode, AssetManager assetManager, PhysicsSpace space, String type) {
         String floorTexturePath = "";
         String wallTexturePath = "";
         
@@ -49,73 +43,83 @@ public class Maze {
         Material wallMaterial = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         wallMaterial.setTexture("ColorMap", assetManager.loadTexture(wallTexturePath));
         
-        //floor
-        for (int i = -25; i <= 25; i++) {
-            for (int j = -25; j <= 25; j++) {
-                Box floorBox = new Box(1, 0.25f, 1);
-                Geometry floorGeometry = new Geometry("Floor", floorBox);
-                floorGeometry.setMaterial(floorMaterial);
-                floorGeometry.setLocalTranslation((i*2), -0.25f, (j*2));
-                floorGeometry.addControl(new RigidBodyControl(0));
-                rootNode.attachChild(floorGeometry);
-                space.add(floorGeometry);
+        //follows a right-handed coordinate system, go right and down to increase in j and i respectively
+        float[][] maze = {{1, 1, 1, 1, 1, 1, 1},
+                        {1, 0, 0, 0, 0, 0, 1},
+                        {1, 0, 0, 0, 0, 0, 1},
+                        {1, 0, 0, 2, 0, 0, 1},
+                        {1, 0, 0, 0, 0, 0, 1},
+                        {1, 0, 0, 0, 0, 0, 1},
+                        {1, 1, 1, 0, 1, 1, 1},
+                        {1, 1, 1, 0, 1, 1, 1},
+                        {1, 1, 1, 0, 1, 1, 1},
+                        {1, 0, 0, 0, 0, 0, 1},
+                        {1, 0, 0, 0, 0, 0, 1},
+                        {1, 0, 0, 0, 0, 0, 1},
+                        {1, 0, 0, 0, 0, 0, 1},
+                        {1, 0, 0, 0, 0, 0, 1},
+                        {1, 1, 1, 1, 1, 1, 1}};
+        
+        //generate floor and ceiling
+        for (int j = 0; j < maze.length; j++) {
+            for (int i = 0; i < maze[j].length; i++) {
+                createFloor(rootNode, assetManager, space, floorMaterial, "floor", i, j);
+                createFloor(rootNode, assetManager, space, floorMaterial, "ceiling", i, j);
             }
         }
         
-        //ceiling
-        for (int i = -25; i <= 25; i++) {
-            for (int j = -25; j <= 25; j++) {
-                Box floorBox = new Box(1, 0.25f, 1);
-                Geometry floorGeometry = new Geometry("Floor", floorBox);
-                floorGeometry.setMaterial(floorMaterial);
-                floorGeometry.setLocalTranslation((i*2), 5.25f, (j*2));
-                floorGeometry.addControl(new RigidBodyControl(0));
-                rootNode.attachChild(floorGeometry);
-                space.add(floorGeometry);
+        float[] returnArray = new float[2];
+        
+        for (int j = 0; j < maze.length; j++) {
+            for (int i = 0; i < maze[j].length; i++) {
+                if (maze[j][i] == 0) { //this is the floor, do nothing
+                }
+                else if (maze[j][i] == 1) { //this is a wall, actually do something
+                    createWall(rootNode, assetManager, space, wallMaterial, i, j);
+                }
+                else if (maze[j][i] == 2) { //this is the starting location, spawn player here
+                    //blocks are in multiples of 2 so gotta multiply it by that same amount
+                    returnArray[0] = (float)i*2.0f;
+                    returnArray[1] = (float)j*2.0f;
+                }
             }
         }
-        
-        //walls
-        for (int j = 0; j <= 2; j++) {
-            for (int i = -25; i <= 25; i++){
-                Box box = new Box(1, 1, 1);
-                Geometry boxGeometry = new Geometry("Box", box);
-                boxGeometry.setMaterial(wallMaterial);
-                boxGeometry.setLocalTranslation((i*2f), (1+j*2f) , -50f);
-                boxGeometry.addControl(new RigidBodyControl(new MeshCollisionShape(box), 0));
-                rootNode.attachChild(boxGeometry);
-                space.add(boxGeometry);
-            }
-            for (int i = -25; i <= 25; i++){
-                //immovable Box with mesh collision shape
-                Box box = new Box(1, 1, 1);
-                Geometry boxGeometry = new Geometry("Box", box);
-                boxGeometry.setMaterial(wallMaterial);
-                boxGeometry.setLocalTranslation((i*2f), (1+j*2f), 50f);
-                boxGeometry.addControl(new RigidBodyControl(new MeshCollisionShape(box), 0));
-                rootNode.attachChild(boxGeometry);
-                space.add(boxGeometry);
-            }
-            for (int i = -25; i <= 25; i++){
-                //immovable Box with mesh collision shape
-                Box box = new Box(1, 1, 1);
-                Geometry boxGeometry = new Geometry("Box", box);
-                boxGeometry.setMaterial(wallMaterial);
-                boxGeometry.setLocalTranslation(-50f, (1+j*2f), (i*2));
-                boxGeometry.addControl(new RigidBodyControl(new MeshCollisionShape(box), 0));
-                rootNode.attachChild(boxGeometry);
-                space.add(boxGeometry);
-            }
-            for (int i = -25; i <= 25; i++){
-                //immovable Box with mesh collision shape
-                Box box = new Box(1, 1, 1);
-                Geometry boxGeometry = new Geometry("Box", box);
-                boxGeometry.setMaterial(wallMaterial);
-                boxGeometry.setLocalTranslation(50f, (1+j*2f), (i*2));
-                boxGeometry.addControl(new RigidBodyControl(new MeshCollisionShape(box), 0));
-                rootNode.attachChild(boxGeometry);
-                space.add(boxGeometry);
-            }
+        return returnArray;
+    }
+    
+    /*Here, I got tired of seeing the same chunk of code dozens of times. Made these methods to condense the lines.*/
+    //method to generate floors
+    public static void createFloor (Node rootNode, AssetManager assetManager, PhysicsSpace space, Material floorMaterial, 
+            String location, int value1, int value2) {
+        int i = value1, j = value2; //i is the taken x coordinate, j in the taken z coordinate.
+        float yLevel = 0; //determines which height the floor tiles are being placed at.
+        if (location.equals("floor")) {
+            yLevel = -0.25f;
+        }
+        else if (location.equals("ceiling")){
+            yLevel = 5.25f;
+        }
+        Box floorBox = new Box(1, 0.25f, 1);
+        Geometry floorGeometry = new Geometry("Floor", floorBox);
+        floorGeometry.setMaterial(floorMaterial);
+        floorGeometry.setLocalTranslation((i*2f), yLevel, (j*2f));
+        floorGeometry.addControl(new RigidBodyControl(0));
+        rootNode.attachChild(floorGeometry);
+        space.add(floorGeometry);
+    }
+    
+    //method to generate walls
+    public static void createWall (Node rootNode, AssetManager assetManager, PhysicsSpace space, Material wallMaterial, 
+            int value1, int value2) {
+        int x = value1, z = value2, y = 0;
+        Box box = new Box(1, 1, 1);
+        for (y = 0; y <= 2; y++) { //stack 3 boxes on each other to simulate a "wall"
+            Geometry boxGeometry = new Geometry("Box", box);
+            boxGeometry.setMaterial(wallMaterial);
+            boxGeometry.setLocalTranslation((x*2f), (1+y*2f) , (z*2f));
+            boxGeometry.addControl(new RigidBodyControl(new MeshCollisionShape(box), 0));
+            rootNode.attachChild(boxGeometry);
+            space.add(boxGeometry);
         }
     }
 }
