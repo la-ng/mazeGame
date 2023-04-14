e94cc7b2cf75d991637c6d7d9c03559ca620f435
davozebi <105819316+davozebi@users.noreply.github.com>
2023-04-14 01:52:04 -0400

    Added start menu option functionality

--- MazeGame/src/main/java/com/mygame/Maze.java
+++ MazeGame/src/main/java/com/mygame/Maze.java
@@ -24,73 +24,98 @@ public class Maze {
      * @param space where collision objects should be added
      */
 
-    public static void createWorld(Node rootNode, AssetManager assetManager, PhysicsSpace space) {
+    public static void createWorld(Node rootNode, AssetManager assetManager, PhysicsSpace space, String type) {
         PointLight light = new PointLight();
         light.setColor(ColorRGBA.Yellow);
         light.setRadius(4f);
         
         rootNode.addLight(light);
+        
+        String floorTexturePath = "";
+        String wallTexturePath = "";
+        
+        if (type.equals("Deepslate")) {
+            floorTexturePath = "Textures/stoneFloor.jpg";
+            wallTexturePath = "Textures/stoneWall.jpg";
+        }
+        else if (type.equals("Stronghold")) {
+            floorTexturePath = "Textures/stone_bricks.png";
+            wallTexturePath = "Textures/stone_bricks.png";
+        }
 
         Material floorMaterial = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        floorMaterial.setTexture("ColorMap", assetManager.loadTexture("Textures/stoneFloor.jpg"));
+        floorMaterial.setTexture("ColorMap", assetManager.loadTexture(floorTexturePath));
         
         Material wallMaterial = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        wallMaterial.setTexture("ColorMap", assetManager.loadTexture("Textures/stoneWall.jpg"));
+        wallMaterial.setTexture("ColorMap", assetManager.loadTexture(wallTexturePath));
         
-        //floor, this is about to get nuked, currently working on this
+        //floor
         for (int i = -25; i <= 25; i++) {
             for (int j = -25; j <= 25; j++) {
-                createFloor(rootNode, assetManager, space, floorMaterial, "floor", i, j);
+                Box floorBox = new Box(1, 0.25f, 1);
+                Geometry floorGeometry = new Geometry("Floor", floorBox);
+                floorGeometry.setMaterial(floorMaterial);
+                floorGeometry.setLocalTranslation((i*2), -0.25f, (j*2));
+                floorGeometry.addControl(new RigidBodyControl(0));
+                rootNode.attachChild(floorGeometry);
+                space.add(floorGeometry);
             }
         }
         
-        //ceiling, this is about to get nuked, currently working on this
+        //ceiling
         for (int i = -25; i <= 25; i++) {
             for (int j = -25; j <= 25; j++) {
-                createFloor(rootNode, assetManager, space, floorMaterial, "ceiling", i, j);
+                Box floorBox = new Box(1, 0.25f, 1);
+                Geometry floorGeometry = new Geometry("Floor", floorBox);
+                floorGeometry.setMaterial(floorMaterial);
+                floorGeometry.setLocalTranslation((i*2), 5.25f, (j*2));
+                floorGeometry.addControl(new RigidBodyControl(0));
+                rootNode.attachChild(floorGeometry);
+                space.add(floorGeometry);
             }
         }
         
-        //walls, I'm working on this rn, this is just a demo. Yes, I nuked a majority of the walls
-        for (int i = -25; i <= 25; i++) {
-            int j = -25;
-            createWall(rootNode, assetManager, space, wallMaterial, i, j);
-        }
-    }
-    
-    /*Here, I got tired of seeing the same chunk of code dozens of times. Made these methods to condense the lines.*/
-    //method to generate floors
-    public static void createFloor (Node rootNode, AssetManager assetManager, PhysicsSpace space, Material floorMaterial, 
-            String location, int value1, int value2) {
-        int i = value1, j = value2; //i is the taken x coordinate, j in the taken z coordinate.
-        float yLevel = 0; //determines which height the floor tiles are being placed at.
-        if (location.equals("floor")) {
-            yLevel = -0.25f;
-        }
-        else if (location.equals("ceiling")){
-            yLevel = 5.25f;
-        }
-        Box floorBox = new Box(1, 0.25f, 1);
-        Geometry floorGeometry = new Geometry("Floor", floorBox);
-        floorGeometry.setMaterial(floorMaterial);
-        floorGeometry.setLocalTranslation((i*2f), yLevel, (j*2f));
-        floorGeometry.addControl(new RigidBodyControl(0));
-        rootNode.attachChild(floorGeometry);
-        space.add(floorGeometry);
-    }
-    
-    //method to generate walls
-    public static void createWall (Node rootNode, AssetManager assetManager, PhysicsSpace space, Material wallMaterial, 
-            int value1, int value2) {
-        int x = value1, z = value2, y = 0;
-        Box box = new Box(1, 1, 1);
-        for (y = 0; y <= 2; y++) { //stack 3 boxes on each other to simulate a "wall"
-            Geometry boxGeometry = new Geometry("Box", box);
-            boxGeometry.setMaterial(wallMaterial);
-            boxGeometry.setLocalTranslation((x*2f), (1+y*2f) , (z*2f));
-            boxGeometry.addControl(new RigidBodyControl(new MeshCollisionShape(box), 0));
-            rootNode.attachChild(boxGeometry);
-            space.add(boxGeometry);
+        //walls
+        for (int j = 0; j <= 2; j++) {
+            for (int i = -25; i <= 25; i++){
+                Box box = new Box(1, 1, 1);
+                Geometry boxGeometry = new Geometry("Box", box);
+                boxGeometry.setMaterial(wallMaterial);
+                boxGeometry.setLocalTranslation((i*2f), (1+j*2f) , -50f);
+                boxGeometry.addControl(new RigidBodyControl(new MeshCollisionShape(box), 0));
+                rootNode.attachChild(boxGeometry);
+                space.add(boxGeometry);
+            }
+            for (int i = -25; i <= 25; i++){
+                //immovable Box with mesh collision shape
+                Box box = new Box(1, 1, 1);
+                Geometry boxGeometry = new Geometry("Box", box);
+                boxGeometry.setMaterial(wallMaterial);
+                boxGeometry.setLocalTranslation((i*2f), (1+j*2f), 50f);
+                boxGeometry.addControl(new RigidBodyControl(new MeshCollisionShape(box), 0));
+                rootNode.attachChild(boxGeometry);
+                space.add(boxGeometry);
+            }
+            for (int i = -25; i <= 25; i++){
+                //immovable Box with mesh collision shape
+                Box box = new Box(1, 1, 1);
+                Geometry boxGeometry = new Geometry("Box", box);
+                boxGeometry.setMaterial(wallMaterial);
+                boxGeometry.setLocalTranslation(-50f, (1+j*2f), (i*2));
+                boxGeometry.addControl(new RigidBodyControl(new MeshCollisionShape(box), 0));
+                rootNode.attachChild(boxGeometry);
+                space.add(boxGeometry);
+            }
+            for (int i = -25; i <= 25; i++){
+                //immovable Box with mesh collision shape
+                Box box = new Box(1, 1, 1);
+                Geometry boxGeometry = new Geometry("Box", box);
+                boxGeometry.setMaterial(wallMaterial);
+                boxGeometry.setLocalTranslation(50f, (1+j*2f), (i*2));
+                boxGeometry.addControl(new RigidBodyControl(new MeshCollisionShape(box), 0));
+                rootNode.attachChild(boxGeometry);
+                space.add(boxGeometry);
+            }
         }
     }
 }
